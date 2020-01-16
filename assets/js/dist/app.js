90f36c442d56b0486f772bfab5d0a32e752ea106
daniel.rojas-at-230160332096 <daniel.rojas@i2btech>
2020-01-16 00:09:54 -0500

    newsletter desktop and mobile

--- assets/js/dist/app.js
+++ assets/js/dist/app.js
@@ -2,6 +2,7 @@ const api_url = 'https://api.jsonbin.io/b/5e1aa891b236b871b3605dd6/1';
 
 var products = document.getElementById('products');
 
+// Load Data
 function getJson() {
     fetch(api_url)
         .then(res => res.json())
@@ -17,6 +18,14 @@ function formatPrice(price) {
     return price
 }
 
+function bestSeller(name) {
+    var name = name.includes('Brazil Cachoeira');    
+    return name;
+}
+
+//console.log(bestSeller('Brazil'))
+
+
 // Load Data Products
 function getProducts(dataJson) {
 
@@ -27,8 +36,15 @@ function getProducts(dataJson) {
     
     const countDataJson = dataJson.data.length;
         for(var i = 0; i < countDataJson; i++ ) {
-            products.innerHTML += `
-                <figure>
+
+            var figure = document.getElementsByClassName('product')
+            
+            if(bestSeller(dataJson.data[i].name)) {
+                products.innerHTML += `
+                <figure class="product">
+                    <span class="best-seller tada">
+                        BEST SELLER
+                    </span>
                     <a href="#" id="${dataJson.data[i].id}">
                         <img src="${dataJson.data[i].image}" alt="${dataJson.data[i].name}">
                         <figcaption>
@@ -40,10 +56,29 @@ function getProducts(dataJson) {
                     </a>
                 </figure>
             `
-            console.log(dataJson.data[i].name)
+            } else {
+                products.innerHTML += `
+                <figure class="product">
+                    <a href="#" id="${dataJson.data[i].id}">
+                        <img src="${dataJson.data[i].image}" alt="${dataJson.data[i].name}">
+                        <figcaption>
+                            <p>${dataJson.data[i].name}</p>
+                            <span>
+                                ${formatPrice(dataJson.data[i].price)}
+                            </span>
+                        </figcaption>
+                    </a>
+                </figure>
+            `
+            }
+
+            
+            //console.log(dataJson.data[i].name)
         }
 
 }
 
 getJson();
+
+
 
